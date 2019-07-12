8845123aba7a9a516e87c4aa0c724fe173b3df93
Olumoroti <olu.anibaba@yahoo.com>
2019-07-12 14:42:40 -0400

    Worked on returning values with return.js

---
+++ Functions/return.js
@@ -0,0 +1,21 @@
+// function foo() {
+//     return "Spoodermun" ;
+//     // return "yee";
+
+// }
+
+// let bar = foo();
+// console.log(bar);
+
+// function randNum(input = 10) {
+//     let decimal = Math.random();
+//     let timesX = decimal * input;
+//     let number = Math.floor(timesX);
+//     return number;
+// }
+
+// let max = process.argv[2];
+// let randomNumber = randNum(max);
+
+// console.log(randomNumber, "is your random number between 0 and", max);
+
