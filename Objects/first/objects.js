704ce64572acfcf64b417ee5789ab26b14df5660
Olumoroti <olu.anibaba@yahoo.com>
2019-07-23 11:13:21 -0400

    Worked on spaceInvadersMVP, html exercises, object practice, etc.

---
+++ Objects/first/objects.js
@@ -0,0 +1,13 @@
+let mo = { 
+    name: "Olumoroti",
+    age: 16,
+    gender: "male"
+}
+
+let countries = {
+    Lesotho: 2233000,
+    North_Korea: 25490000,  
+    Paraguay: 6811000,
+    Denmark: 5806000,
+    Japan: 126800000
+}
